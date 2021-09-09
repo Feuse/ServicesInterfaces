70c77595d4b9ff86a8a648896f006525b3ac5d75
Roman Sterlin <Feuse132@gmail.com>
2021-09-09 16:03:00 +0300

    update

--- IDataAccess.cs
+++ IDataAccess.cs
@@ -15,9 +15,9 @@ namespace ServicesInterfaces
         public Task<UpdateResult> RegisterService(Data data);
         public Task UpdateServiceSession(Data data);
         public Task<ServiceSessions> CheckForServiceSession(Data data);
-        public Task<UserServiceCredentials> GetUserServiceByServiceName(Data data);
-        public Task<List<UserServiceCredentials>> GetAllUserServicesById(Data data);
-        public Task<UpdateResult> RemoveServiceFromUser(Data data);
+        public Task<UserServiceCredentials> GetUserServiceByServiceNameAndId(Data data);
+        public Task<List<UserServiceCredentials>> GetAllUserServicesById(string id);
+        public Task<DeleteResult> RemoveServiceFromUser(Data data);
         public Task<UserCredentials> RegisterUser(Data data);
         public Task<UserCredentials> AuthenticateUser(Data data);
         public Task<UserCredentials> CheckIfUsernameExists(Data data);
